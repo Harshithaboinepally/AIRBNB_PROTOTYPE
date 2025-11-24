ffdedb66526111a4872d86219bd2a17c10d79665
Shriansh Chari <30420527+shrianshChari@users.noreply.github.com>
2025-11-24 11:08:08 -0800

    Part 1 of MongoDB conversion

--- backend/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dashboardController = require('../controllers/dashboardController');
-const { authenticateJWT, isTraveler, isOwner } = require('../middleware/auth');
-
-router.get('/traveler', authenticateJWT, isTraveler, dashboardController.getTravelerDashboard);
-router.get('/owner', authenticateJWT, isOwner, dashboardController.getOwnerDashboard);
-
-module.exports = router;
